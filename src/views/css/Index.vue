1e9e1e10354c663156d8748550dee49f4ca03c23
Mizu <psdk55052004@gmail.com>
2024-01-24 16:10:54 +0800

    新增 CSS預設頁面

--- src/views/css/Index.vue
+++ src/views/css/Index.vue
@@ -5,6 +5,19 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+onMounted(() => {
+  goToDefaultPage()
+})
+
+function goToDefaultPage () {
+  if (route.name === 'css') router.push('/css/flex')
+}
 </script>
 
 <style lang='scss' scope></style>
