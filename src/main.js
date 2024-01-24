841f50ec5b622dcbc96f4e5c4f2be63009655520
Mizu <psdk55052004@gmail.com>
2024-01-24 15:17:17 +0800

    匯入 vuetify, vuetify font

--- src/main.js
+++ src/main.js
@@ -7,10 +7,14 @@ import 'bootstrap'
 
 import App from './App.vue'
 
+// Vuetify
+import { vuetify } from '@/modules/vuetify.js'
+
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
+app.use(vuetify)
 
 app.mount('#app')
