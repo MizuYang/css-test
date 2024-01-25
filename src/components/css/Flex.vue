f6c4190a8949156a31c55bd3f049995a4e2444dc
Mizu <psdk55052004@gmail.com>
2024-01-25 10:16:56 +0800

    新增 使用 defineAsyncComponent 動態匯入元件

---
+++ src/components/css/Flex.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+ flex-template
+</template>
+
+<style lang='scss' scope></style>
--- src/views/css/Index.vue
+++ src/views/css/Index.vue
@@ -1,22 +1,45 @@
 <template>
-  <p class="text-primary-color position-absolute-center">
-    css
-  </p>
+  <main class="container">
+    <!-- <DemoBox /> -->
+    <template v-for="item in demoData" :key="item?.name">
+      <template v-if="item.component.name">
+        <component :is="item?.component"></component>
+      </template>
+    </template>
+  </main>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
+import { reactive, defineAsyncComponent, markRaw, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
+// data
 const route = useRoute()
 const router = useRouter()
+const demoData = reactive({
+  flex: {
+    fileName: 'Flex',
+    component: ''
+  },
+  flex2: {
+    fileName: 'Flex2',
+    component: ''
+  }
+})
 
-onMounted(() => {
+onMounted(async () => {
   goToDefaultPage()
+  await getComponents()
 })
 
 function goToDefaultPage () {
   if (route.name === 'css') router.push('/css/flex')
+}
+async function getComponents () {
+  Object.keys(demoData).forEach(key => {
+    const name = demoData[key].fileName
+    demoData[key].component = markRaw(defineAsyncComponent(() => import(`../../components/css/${name}.vue`)))
+  })
 }
 </script>
 
