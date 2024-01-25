fd24004c7fdbbc5edecc1f899addd4391b07e2fb
Mizu <psdk55052004@gmail.com>
2024-01-25 14:05:58 +0800

    新增 input 共用元件, 並用 movelValue 父子雙向綁定變數

---
+++ src/components/Input.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  modelValue: {
+    type: String,
+    default: 'gg'
+  }
+})
+</script>
+
+<template>
+  <div class='form-floating mb-3'>
+    <input type='text'
+           class='form-control'
+           id='floatingInput'
+           placeholder='輸入語法'
+           :value="props.modelValue"
+           @input="$emit('update:modelValue', $event.target.value)"
+           style="width:300px;">
+    <label for='floatingInput'>輸入 <code>{{ props.title }}</code> 語法</label>
+  </div>
+</template>
+
+<style lang='scss' scope></style>
