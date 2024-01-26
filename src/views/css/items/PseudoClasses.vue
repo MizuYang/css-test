2db6b43f948adb71e6ea614c78ec7529700907a8
Mizu <psdk55052004@gmail.com>
2024-01-26 15:11:27 +0800

    defineAsyncComponent 匯入所有 demo 元件

--- src/views/css/items/PseudoClasses.vue
+++ src/views/css/items/PseudoClasses.vue
@@ -1,9 +1,41 @@
 <script setup>
-console.log('pseudoClasses')
+import { reactive, onMounted, markRaw, defineAsyncComponent } from 'vue'
+
+const demoData = reactive({ // eslint-disable-line
+  checked: {
+    fileName: 'Checked',
+    title: 'checked',
+    // content: '設定 Flex 元素在主軸方向上的初始大小',
+    component: ''
+  }
+})
+
+onMounted(async () => {
+  await getComponents()
+})
+
+async function getComponents () {
+  Object.keys(demoData).forEach(key => {
+    const { fileName } = demoData[key]
+    demoData[key].component = markRaw(defineAsyncComponent(() => (import(`../../../components/css/pseudoClasses/${fileName}.vue`))))
+  })
+}
 </script>
 
 <template>
-偽類
+  <template v-for="item in demoData" :key="item?.fileName">
+    <template v-if="item?.component">
+      <component :is="item?.component" class="border p-5">
+        <!-- <template #title>
+          <p><code>{{ item.title }}：</code></p>
+        </template>
+        <template #content>
+          <p>{{ item.content }}</p>
+        </template> -->
+      </component>
+    </template>
+  </template>
+  <pre>{{ demoData }}</pre>
 </template>
 
 <style lang='scss' scope></style>
