15a202db93f6af4fbd7bbde1fa101335fc5b5d78
Mizu <psdk55052004@gmail.com>
2024-02-01 11:45:24 +0800

    新增 ::marker

---
+++ src/components/css/pseudoElements/Marker.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <section class="mt-5 my-2">
+    <div class="d-flex aling-items-center mb-2">
+      <slot name="title"></slot>
+      <slot name="content"></slot>
+    </div>
+
+    <ul style="margin-left:200px;">
+      <li class="test-marker">1</li>
+      <li class="test-marker">2</li>
+      <li class="test-marker">3</li>
+    </ul>
+  </section>
+</template>
+
+<style lang='scss' scope>
+.test-marker::marker {
+  content: '(我是:marker) - ';
+  color: red;
+}
+</style>
